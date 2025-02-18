ef15542fed4534d1a22db90164ee54f15311156d
Denis <d.m.2005qqq@gmail.com>
2025-02-18 09:55:52 +0300

    chore: comments added

--- src/twitterScraper.py
+++ src/twitterScraper.py
@@ -13,118 +13,137 @@ ua = UserAgent()
 
 class Scraper:
     def __init__(self, chromedriver_path: str, user_agent: str = ua.chrome, manual_delay: int = 60) -> None:
-        self.user_agent = user_agent
-        self.scraped_data = list()
-        self.chromedriver_path = chromedriver_path
+        # Инициализация экземпляра класса Scraper с заданными параметрами
+        self.user_agent = user_agent  # Пользовательский агент для имитации браузера
+        self.scraped_data = list()  # Список для сохранения собранных данных
+        self.chromedriver_path = chromedriver_path  # Путь к исполняемому файлу ChromeDriver
         
-        self._login(manual_delay)
-        self.driver = self._create_driver()
+        # Выполняем вход в аккаунт перед созданием драйвера
+        self._login(manual_delay)  
+        self.driver = self._create_driver()  # Создаем веб-драйвер для взаимодействия с браузером
         
-    def _create_driver(self, headless:bool = False) -> webdriver.Chrome:
-        chrome_options = webdriver.ChromeOptions()
-        service = Service(executable_path=self.chromedriver_path)
-        if headless: chrome_options.add_argument('--headless')
-        
-        system = platform.system()
+    def _create_driver(self, headless: bool = False) -> webdriver.Chrome:
+        ''' Метод для создания драйвера Chrome'''
+        chrome_options = webdriver.ChromeOptions()  # Опции для конфигурации Chrome
+        service = Service(executable_path=self.chromedriver_path)  # Сервис для запуска ChromeDriver
+        if headless:
+            chrome_options.add_argument('--headless')  # Включает безголовый режим, если задано
+
+        system = platform.system()  # Определяем операционную систему
         if system == "Windows":
+            # Указываем путь к пользовательским данным для Windows
             chrome_options.add_argument(r'user-data-dir=C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data\Default')
         elif system == "Darwin":
+            # Указываем путь к пользовательским данным для macOS
             chrome_options.add_argument('user-data-dir=./Library/Application Support/Google/Chrome/Default')
         else:
-            raise Exception("Unsupported operating system")
+            raise Exception("Unsupported operating system")  # Обрабатываем неподдерживаемую ОС
         
-        chrome_options.add_argument(f'user-agent={self.user_agent}')
-        driver = webdriver.Chrome(options=chrome_options, service=service)
+        chrome_options.add_argument(f'user-agent={self.user_agent}')  # Устанавливаем пользовательский агент
+        driver = webdriver.Chrome(options=chrome_options, service=service)  # Создаем экземпляр драйвера
         return driver
         
     def _login(self, manual_delay: int) -> None:
-        temp_driver = self._create_driver()
-        temp_driver.get("https://twitter.com/login")
-        time.sleep(manual_delay)
-        temp_driver.quit()
+        '''Метод для выполнения входа в Twitter с временной задержкой для ручного ввода'''
+        temp_driver = self._create_driver()  # Создаем временный драйвер
+        temp_driver.get("https://twitter.com/login")  # Переходим на страницу входа в Twitter
+        time.sleep(manual_delay)  # Задержка для ввода учетных данных вручную
+        temp_driver.quit()  # Закрываем временный драйвер
 
-    def scrape_request(self, q: str, since: str, until: str, recent: bool = True, exculde_replies: bool = True) -> list[dict]:
-        assert until > since
-    
+    def scrape_request(self, q: str, since: str, until: str, recent: bool = True, exclude_replies: bool = True) -> list[dict]:
+        '''Метод для сбора твитов по заданному запросу'''
+        assert until > since  # Проверяем, что 'until' позже 'since'
+
+        # Формируем URL для поиска с заданными параметрами
         self.driver.get(f'https://twitter.com/search?q={q}%20until%3A{until}%20since%3A{since}'
-                        + '%20-filter%3Areplies'*exculde_replies
+                        + '%20-filter%3Areplies'*exclude_replies
                         + '&src=recent_search_click'
                         + '&f=live'*recent)
         
-        scrollDelay = 0.1  # Delay between each scroll
+        scrollDelay = 0.1  # Задержка между скроллингом страниц
         
-        time.sleep(20)
+        time.sleep(20)  # Задержка для загрузки контента
         
+        # Обработка ошибки: если страница загружается неправильно
         while 'Something went wrong' in self.driver.page_source:
-            sleep(30)
-            self.driver.refresh()
-            sleep(5)
+            sleep(30)  # Задержка перед повторной попыткой
+            self.driver.refresh()  # Обновляем страницу
+            sleep(5)  # Задержка после обновления
         
         try:
-            articles = self.driver.find_elements(By.XPATH,"//article[@data-testid='tweet']") 
+            # Поиск всех твитов на странице по указанному XPATH
+            articles = self.driver.find_elements(By.XPATH, "//article[@data-testid='tweet']")
         except:
+            # Обработка ошибок при поиске
             raise Exception(RuntimeError)
         
-        if len(articles) == 0:
+        if len(articles) == 0:  # Если твиты не найдены
             if 'No results for' in self.driver.page_source:   
-                return []
-            else: return None
+                return []  # Возвращаем пустой список, если результатов нет
+            else: 
+                return None
             
-        tweets = []
+        tweets = []  # Список для сохранения найденных твитов
                 
-        i = 100
-        counter = 0
+        i = 100  # Количество пикселей для прокрутки
+        counter = 0  # Счетчик для отслеживания количества прокруток без обновления
+        
         while True:
-            for article in articles:
-                try:
-                    tag = article.find_element(By.XPATH,".//div[@data-testid='User-Name']").text
-                    ts = article.find_element(By.XPATH,".//time").text
-                    tweet = article.find_element(By.XPATH,".//div[@data-testid='tweetText']").text.strip().replace('\n', ' ')
-                    reply = article.find_element(By.XPATH,".//div[@data-testid='reply']").text
-                    retweet = article.find_element(By.XPATH,".//div[@data-testid='retweet']").text
-                    like = article.find_element(By.XPATH,".//div[@data-testid='like']").text
-                    #view = article.find_element(By.XPATH, "//div[@role='group']//a").text
+            for article in articles:  # Цикл по найденным твитам
+                try:  # Пытаемся извлечь нужные данные
+                    ts = article.find_element(By.XPATH, ".//div[@data-testid='User-Name']").text  # Имя пользователя
+                    tag = article.find_element(By.XPATH, ".//time").text  # Время публикации
+                    tweet = article.find_element(By.XPATH, ".//div[@data-testid='tweetText']").text.strip().replace('\n', ' ')  # Текст твита
+                    reply = article.find_element(By.XPATH, ".//div[@data-testid='reply']").text  # Количество ответов
+                    retweet = article.find_element(By.XPATH, ".//div[@data-testid='retweet']").text  # Количество ретвитов
+                    like = article.find_element(By.XPATH, ".//div[@data-testid='like']").text  # Количество лайков
                     
+                    # Добавляем данные твита в список
                     tweets.append({'UserTag': ts, 
-                    'TimeStamp': tag,
-                    'Tweet': tweet,
-                    'Replies': reply,
-                    'reTweets': retweet, 
-                    'Likes': like,
-                    #'Views': view,
-                    })
+                                   'TimeStamp': tag,
+                                   'Tweet': tweet,
+                                   'Replies': reply,
+                                   'reTweets': retweet, 
+                                   'Likes': like,
+                                   })
                 except:
-                    pass
-                    
+                    pass  # Игнорируем ошибки при извлечении данных
             
-            last_height = self.driver.execute_script("return window.pageYOffset")
+            last_height = self.driver.execute_script("return window.pageYOffset")  # Запоминаем текущую высоту прокрутки
             
+            # Скроллим вниз на заданное количество пикселей
             self.driver.execute_script(f"window.scrollBy(0, {i})")
-            self.driver.implicitly_wait(scrollDelay)
+            self.driver.implicitly_wait(scrollDelay)  # Ждем в соответствии с заданной задержкой
             
-            new_height = self.driver.execute_script("return window.pageYOffset")
+            new_height = self.driver.execute_script("return window.pageYOffset")  # Новая высота прокрутки
             
+            # Если высота прокрутки не изменилась, увеличиваем счетчик
             if last_height == new_height:
                 counter += 1
-                self.driver.implicitly_wait(scrollDelay*10)
+                self.driver.implicitly_wait(scrollDelay * 10)  # Увеличиваем задержку, если ничего не загружено
             else:
-                counter = 0
+                counter = 0  # Сбрасываем счетчик, если есть новые данные
                 
+            # Задержка перед следующей прокруткой, если данных нет
             if counter == 1:
                 sleep(5)
             
+            # Выходим из цикла после 5 последовательных прокруток без загрузки новых данных
             if counter == 5:
                 break
             
             try:
-                articles = self.driver.find_elements(By.XPATH,"//article[@data-testid='tweet']") 
+                # Обновляем список статей твитов на странице
+                articles = self.driver.find_elements(By.XPATH, "//article[@data-testid='tweet']") 
             except:
-                pass
+                pass  # Игнорируем ошибки при повторном поиске статей
         
-        self.scraped_data+=tweets
+        # Объединяем собранные твиты с уже существующими данными
+        self.scraped_data += tweets
+        # Убираем дубликаты из общей коллекции собранных данных
         self.scraped_data = [dict(t) for t in {tuple(d.items()) for d in self.scraped_data}]
-        return tweets
+        return tweets  # Возвращаем список собранных твитов
 
     def clean_buffer(self) -> None:
-        self.scraped_data.clear()
-        
+        '''Метод для очистки буффера'''
+        self.scraped_data.clear()  # Очищаем список собранных данных
